8b015bccf0c8b344f14b3af1cdbacabb0a211b36
cybiz <49467492+cybiz-tva@users.noreply.github.com>
2024-06-12 08:54:46 +0530

    heading

--- src/pages/home/ui/HomePage.tsx
+++ src/pages/home/ui/HomePage.tsx
@@ -33,9 +33,9 @@ const MainBlock = () => (
   <section className={styles.mainBlock}>
     <div className={styles.ctaContainer}>
       <header className={styles.header}>
-        <Heading theme="auto" level="h1" className={styles.text}>
+        <Heading theme="auto" level="h3" className={styles.text}>
           Most Trusted{' '}
-          <span className={styles.blueText}>Withdrwal & &nbsp;Deposits</span>
+          <span className={styles.blueText}>withdrwals &&nbsp;Deposits</span>
         </Heading>
       </header>
       <main className={styles.main}>
